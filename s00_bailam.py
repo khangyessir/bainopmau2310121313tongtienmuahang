5144bfdcc425b43a54f58ea2195099fe413a0f2f
khangyessir <146812226+khangyessir@users.noreply.github.com>
2023-10-12 07:01:46 +0000

    nopbai

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,5 @@ tongtienmuahang(None)                   | None
 
 #region bailam
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+  return gia_truoc_thue * 1.1 if gia_truoc_thue else None
 #endregion bailam
